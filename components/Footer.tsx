c3f8e58a5f5bcbcca9ebf8af30af62bed6443916
jamesjimenezzz <asunahaley@gmail.com>
2025-09-11 06:23:47 +0800

    added key

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,8 +7,8 @@ const footerData =["BSIT-01", "Purposive Communication", "Group 4"]
   return (
     <div className='flex sm:flex-row sm:justify-between w-full border-t flex-col   '>
 
-    {footerData.map((f: string) => (
-        <p className='text-muted-foreground mt-2.5'>{f}</p>
+    {footerData.map((f: string, i) => (
+        <p key={i} className='text-muted-foreground mt-2.5'>{f}</p>
     ))}
     </div>
   )
