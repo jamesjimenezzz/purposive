b97da16f4d9327133bb09261b117108548f75318
jamesjimenezzz <asunahaley@gmail.com>
2025-09-11 06:56:09 +0800

    changed image

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import data from "../data.json"
 import { dataCamapings } from '../index'
 import Data from './Data'
 import Image from 'next/image'
-import campaign from "../public/campaign3.png"
+import campaign from "../public/campaign4.png"
 import Footer from '@/components/Footer'
 import Link from 'next/link'
 import {motion} from "framer-motion"
